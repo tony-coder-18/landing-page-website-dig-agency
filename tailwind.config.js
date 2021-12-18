f307f36bb1ea0fa3d102deb55b5509d76167b442
bill <bill.llach1801@gmail.com>
2021-12-17 19:18:14 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#1A1F25',
+        'txt': '#FFFFFF',
+        'darker-txt': '#FFFFFFB2',
+        'nav-txt': '#4C535A',
+        'btn-submit': '#3898EC',
+        'icon-green': '#F7FCF6',
+        'icon-blue': '#F5F9FF',
+        'btn-blue': '#3898EC'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'",'sans-serif']
+      }
+    }
+  }
+}
